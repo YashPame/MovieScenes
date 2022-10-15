d7e44bf29fa32e7f432394cb01fe5f7ae66bfa05
YashPame <88240082+YashPame@users.noreply.github.com>
2022-10-15 16:37:50 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -5,5 +5,11 @@ Data = [
         "image": "IASCLogo.png",
         "Context": "This is a collection of iconic movie scenes and posters.",
     },
+    {
+        "id": 1,
+        "Movie Name": "Baahubali: The Beginning",
+        "image": "https://img.mensxp.com/media/content/2019/Oct/stunning-spots-from-iconic-bollywood-movie-scenes-1200x900-1572081978.jpg",
+        "Context": "",
+    },
 
 ]
